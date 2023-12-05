39fa29ffba593cac43dbd3d0dc5a333aef9a5a46
achraf <36072352+achrafAa@users.noreply.github.com>
2023-12-05 06:26:42 +0100

    Add Database Seeder

---
+++ src/Database/Seeders/DatabaseSeeder.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Database\Seeders;
+use App\Models\Cart;
+use App\Models\Product;
+use Faker\Factory;
+use Illuminate\Database\Capsule\Manager as DBCapsule;
+
+class DatabaseSeeder
+{
+    public function run()
+    {
+        // Seed the database with products
+        $faker = Factory::create();
+        $products = [];
+        for ($i = 0; $i < 10; $i++) {
+            $products[] = [
+                'title' => $faker->sentence(3),
+                'description' => $faker->paragraph(3),
+                'image' => $faker->imageUrl(),
+                'price' => $faker->randomFloat(2, 10, 100),
+            ];
+        }
+        DBCapsule::table('products')->insert($products);
+
+        $carts = [];
+        // Seed the database with carts
+        foreach (range(1, 10) as $i) {
+            $carts[] = [
+                'customer_email' => $faker->email,
+                'customer_fullname' => $faker->name,
+            ];
+        }
+        DBCapsule::table('carts')->insert($carts);
+
+        // Seed the database with cart items
+
+        $carts = Cart::inRandomOrder()->get();
+        $products = Product::inRandomOrder()->limit(10)->pluck('id')->toArray();
+        $cartItems = [];
+        foreach($carts as $cart) {
+            foreach (range(1, 5) as $i) {
+                $cartItems[] = [
+                    'product_id' => $products[rand(0, 9)],
+                    'quantity' => $faker->randomDigit(),
+                    'price' => $faker->randomFloat(2, 10, 100),
+                ];
+            }
+            $cart->items()->createMany($cartItems);
+        }
+    }
+
+}
